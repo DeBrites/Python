becaa1d71cc47817de6327a915d4267e16ff9ce5
OdeBrites <fernandes.caio1@gmail.com>
2025-12-08 14:59:49 -0300

    Começando a aula 1
    Desenvolvendo a teoria básica

---
+++ Mundo 1/Aula1_PrimeirosComandos.py
@@ -0,0 +1,23 @@
+Os dados, se eles forem mensagens, eles terão delimitadores especiais. 
+
+O delimitador especial no Python é a aspa simples (') ou a aspa dupla ("). A grande parte da comunidade utiliza a aspa simples para delimitar mensagens.
+                                                   
+Todos os comandos são funções e todas as funções possuem parênteses.    
+
+Exemplo:
+print('Olá, Mundo!') aparecerá a mensagem Olá, Mundo! na tela.
+
+No Python, números não precisam de aspas. Eles são reconhecidos como números automaticamente.
+
+Exemplo:
+print(7+4) resultará na soma dos números, ou seja, 11.
+
+Porém para expressar números como mensagens, é necessário o uso de aspas.
+
+Exemplo:
+print('7+4') resultará na mensagem '7+4' e não na soma dos números.
+
+E se isolar as aspas em cada número?
+
+Exemplo:
+print('7'+'4') resultará na junção das mensagens, ou seja, '74'.
