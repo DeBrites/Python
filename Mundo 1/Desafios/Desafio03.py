2e15db3532944caf61b4df7823a27e6405594fce
Caio de Brites <62755518+DeBrites@users.noreply.github.com>
2025-12-13 14:17:45 -0300

    Add Python script to sum two numbers

---
+++ Mundo 1/Desafios/Desafio03.py
@@ -0,0 +1,5 @@
+# Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+numero01 = int(input('Qual é o primeiro número?'))
+numero02 = int(input('Qual é o segundo número?'))
+soma = numero01 + numero02
+print('A soma dos dois números é igual a', soma)
