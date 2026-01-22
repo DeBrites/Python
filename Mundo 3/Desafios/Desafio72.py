5a6a2a93f0577881fea6958d7fa485a259bf57fa
OdeBrites <fernandes.caio1@gmail.com>
2026-01-21 22:21:18 -0300

    Desafios 72 e 73
    Desafios concluídos do mundo 3 de Python.

--- Mundo 3/Desafios/Desafio72.py
+++ Mundo 3/Desafios/Desafio72.py
@@ -1,2 +1,18 @@
 # Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+numeros = ('zero','um', 'dois', 'três', 'quatro', 
+           'cinco', 'seis', 'sete', 'oito', 'nove', 
+           'dez', 'onze', 'doze', 'treze', 'catorze', 
+           'quinze', 'dezesseis', ' dezessete', 'dezoito', 
+           'dezenove', 'vinte')
+while True:
+    continuar = ' '
+    escolha = int(input('Escolha um número de 0 a 20: '))
+    if 0 <= escolha <= 20:
+        print(f'O número que você escolheu foi {numeros[escolha]}.')
+        while continuar not in 'SN':
+            continuar = str(input('Você quer continuar? [S/N] ')).strip().upper()[0]
+        if continuar == 'N':
+            break
+    print('Tente mais uma vez.', end = ' ')
+print(f'Programa finalizado. Muito obrigado!')
--- Mundo 3/Desafios/Desafio73.py
+++ Mundo 3/Desafios/Desafio73.py
@@ -4,3 +4,17 @@
 # B) Os últimos 4 colocados da tabela.
 # C) Uma lista com os times em ordem alfabética.
 # D) Em que posição na tabela está o time da Chapecoense.
+brasileirao2026 = ('Atlético-MG', 'Botafogo', 'Athletico-PR', 'Chapecoense', 
+                   'Coritiba', 'Flamengo', 'Vasco da Gama', 'Cruzeiro', 'Bahia', 
+                   'EC Vitória', 'Fluminense', 'Grêmio', 'Mirassol', 'Bragantino', 
+                   'Remo', 'Santos', 'São Paulo', 'Corinthians', 'Internacional', 'Palmeiras')
+print('-=' * 20)
+print(f'Lista de times do Brasileirão: {brasileirao2026}')
+print('-=' * 20)
+print('Os primeiros 5 colocados são: ', brasileirao2026[0:6])
+print('-=' * 20)
+print('Os últimos 4 colocados são: ', brasileirao2026[-4::])
+print('-=' * 20)
+print('Os times em ordem alfábetica são: ', sorted(brasileirao2026))
+print('-=' * 20)
+print(f'O time Chapecoense está na {brasileirao2026.index('Chapecoense') + 1}º posição.')
