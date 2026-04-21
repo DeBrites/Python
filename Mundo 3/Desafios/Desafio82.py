59072be590199eab865f5264bf86d0a359d38683
OdeBrites <fernandes.caio1@gmail.com>
2026-04-21 15:04:50 -0300

    Desafios concluídos
    Realizados os desafios 82 e 83

--- Mundo 3/Desafios/Desafio82.py
+++ Mundo 3/Desafios/Desafio82.py
@@ -2,3 +2,19 @@
 # Depois disso, crie duas listas extras que vão conter apenas os valores pares
 # e os valores ímpares digitados, respectivamente.
 # Ao final, mostre o conteúdo das três listas geradas.
+lista = []
+listapar = lista[:]
+listaimpar = lista[:]
+while True:
+    lista.append(int(input('Digite um número: ')))
+    resposta = input('Quer continuar [s/n] ? ')
+    if resposta in 'Nn':
+        break
+print(f'A lista completa é {lista}')
+for c, d in enumerate(lista):
+    if d % 2 == 0:
+        listapar.append(d)
+    else:
+        listaimpar.append(d)
+print(f'A lista de pares é {listapar}')
+print(f'A lista de ímpares é {listaimpar}')
--- Mundo 3/Desafios/Desafio83.py
+++ Mundo 3/Desafios/Desafio83.py
@@ -1,3 +1,18 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 # Seu aplicativo deverá analisar se a expressão passada está
 # com os parênteses abertos e fechados na ordem correta.
+lista = []
+expressao = input('Digite a expressão: ')
+for simbolo in expressao:
+    if simbolo == '(':
+        lista.append('(')
+    elif simbolo == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print('A expressão está correta!')
+else: 
+    print('A expressão está errada!')
