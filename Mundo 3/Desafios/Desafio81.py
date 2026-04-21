a0340461182ca916b05ea6b8ae3200bc5c4af06c
OdeBrites <fernandes.caio1@gmail.com>
2026-04-21 10:54:24 -0300

    Atualização de desafios
    Realizado o desafio 81

--- Mundo 3/Desafios/Desafio81.py
+++ Mundo 3/Desafios/Desafio81.py
@@ -3,3 +3,17 @@
 # A) Quantos números foram digitados.
 # B) A lista de valores, ordenada de forma decrescente.
 # C) Se o valor 5 foi digitado e está ou não na lista.
+lista = []
+while True:
+    lista.append(int(input('Digite um número: ')))
+    resposta = input('Quer continuar [s/n] ? ')
+    if resposta in 'Nn':
+        break
+print('-='*20)
+print(f'Você digitou {len(lista)} números.')
+lista.sort(reverse=True)
+print(f'A lista em ordem decrescente é {lista}.')
+if 5 in lista:
+    print('O número 5 está na lista.')
+else:
+    print('O número 5 não está na lista.')
